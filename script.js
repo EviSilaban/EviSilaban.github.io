779a58fb983a615ea94deae66c8dc9750038e4a5
EviSilaban <evisilaban75@gmail.com>
2022-04-17 19:12:20 +0700

    my portofolio update

--- script.js
+++ script.js
@@ -22,12 +22,12 @@ const form = document.forms['formPendaftaran'];
 
 const btnKirim = document.querySelector('.btn-kirim');
 const btnLoading = document.querySelector('.btn-loading');
-      
+
 form.addEventListener('submit', e => {
     e.preventDefault()
     btnLoading.classList.toggle('d-none');
     btnKirim.classList.toggle('d-none');
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
         .then(response => {
             btnLoading.classList.toggle('d-none');
             btnKirim.classList.toggle('d-none');
@@ -36,58 +36,25 @@ form.addEventListener('submit', e => {
             console.log('Success!', response)
         })
         .catch(error => console.error('Error!', error.message))
-    })
+})
 
-// teks typing
-const typedTextSpan = document.querySelector(".typed-text");
-const cursorSpan = document.querySelector(".cursor");
-  
-const textArray = ["Evi Rosalina Silaban"];
-const typingDelay = 100;
-const erasingDelay = 100;
-const newTextDelay = 2500;
-let textArrayIndex = 0;
-let charIndex = 0;
-  
-function type() {
-    if (charIndex < textArray[textArrayIndex].length) {
-        if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-            typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-            charIndex++;
-            setTimeout(type, typingDelay);
-        } 
-        else {
-            cursorSpan.classList.remove("typing");
-            setTimeout(erase, newTextDelay);
-        }
-    }
-  
-function erase() {
-    if (charIndex > 0) {
-        if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-            typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
-            charIndex--;
-            setTimeout(erase, erasingDelay);
-        } 
-        else {
-            cursorSpan.classList.remove("typing");
-            textArrayIndex++;
-            if(textArrayIndex>=textArray.length) textArrayIndex=0;
-            setTimeout(type, typingDelay + 1100);
-        }
-    }
-  
-    document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
-        if(textArray.length) setTimeout(type, newTextDelay + 250);
-});
 
-// scroll navbar
-document.addEventListener("scroll", function () {
-    const navbar = document.querySelector("nav");
+// navbar
+const navactive = document.querySelectorAll('.nav-link');
+const section = document.querySelectorAll('section');
 
-      if (this.body.scrollTop > 1 || this.documentElement.scrollTop > 1) {
-        navbar.classList.add("nav-scrolled", "fixed-top");
-      } else {
-        navbar.classList.remove("nav-scrolled", "fixed-top");
-      }
-  });
+function activenav() {
+    let len = section.length;
+    while (--len && window.scrollY + 97 < section[len].offsetTop) { }
+    navactive.forEach(ltx => ltx.classList.remove('active'));
+    navactive[len].classList.add('active');
+}
+activenav();
+window.addEventListener('scroll', activenav);
+
+// download
+const buttondownload = document.querySelector('.download');
+function download() {
+    let download = 'download/Evi Rosalina Silaban.pdf';
+    window.location.href = download;
+}
